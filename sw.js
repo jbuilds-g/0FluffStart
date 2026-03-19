acb92bdb0d18461e12f5341992d57f8b46608b76
JBuilds <goldmanyanky72@gmail.com>
2026-03-19 14:53:10 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// sw.js - v3 PWA Cache
+// sw.js - v4 PWA Cache
 
 const CACHE_NAME = '0fluff';
 const ASSETS = [
@@ -12,7 +12,7 @@ const ASSETS = [
     './utilities.js',
     './ui-logic.js',
     './icon.png'
-    './manifest.json'
+    
 ];
 
 self.addEventListener('install', (e) => {
