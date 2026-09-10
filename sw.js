5d0026b2f2c0b2b8aee1a63b15e8e1801901514d
JBuilds <goldmanyanky72@gmail.com>
2026-09-10 16:43:12 -0400

    fix: cache settings layout module

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "0fluffstart-cache-v1.6.8";
+const CACHE_NAME = "0fluffstart-cache-v1.6.9";
 
 const CORE_APP_SHELL = [
   "./",
@@ -31,6 +31,7 @@ const CORE_APP_SHELL = [
   "./js/material-you-engine.js",
   "./js/utils.js",
   "./js/version.js",
+  "./js/settings-layout.js",
 ];
 
 self.addEventListener("message", (event) => {
