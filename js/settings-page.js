ef2fd676d3aec20f40f4f4df4da64e6ab4ef824d
JBuilds <goldmanyanky72@gmail.com>
2026-09-20 12:36:58 -0400

    Refresh settings stylesheet cache version

--- js/settings-page.js
+++ js/settings-page.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const pageStyles = document.querySelector(
     'link[href^="css/settings-page.css"]',
   );
-  if (pageStyles) pageStyles.href = "css/settings-page.css?v=7";
+  if (pageStyles) pageStyles.href = "css/settings-page.css?v=8";
 
   const controlStyles = document.createElement("link");
   controlStyles.rel = "stylesheet";
